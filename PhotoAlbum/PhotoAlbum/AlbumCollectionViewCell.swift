5b3ba913b514c4d8242da894a97a14246a09bb2e
asqw887 <asqw887@gmail.com>
2022-03-21 21:06:07 +0900

    add - albumCollectionViewCell

---
+++ PhotoAlbum/PhotoAlbum/AlbumCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumCollectionViewCell.swift
+//  PhotoAlbum
+//
+//  Created by 최예주 on 2022/03/21.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    
+    static let cellID = "albumCell"
+    
+}
