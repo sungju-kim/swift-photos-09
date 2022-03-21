918762b4fa7d27a20fd99ed65a570b1b86603a56
asqw887 <asqw887@gmail.com>
2022-03-21 21:49:58 +0900

    feature : ViewController CollectionView DataSource, Delegate 설정

--- PhotoAlbum/PhotoAlbum/ViewController.swift
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var albumCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        albumCollectionView.register(AlbumCollectionViewCell.self, forCellWithReuseIdentifier: AlbumCollectionViewCell.cellID)
+        
+        albumCollectionView.delegate = self
+        albumCollectionView.dataSource = self
+        
+        
     }
 
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AlbumCollectionViewCell.cellID, for: indexPath) as? AlbumCollectionViewCell else { return UICollectionViewCell()}
+        
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+}
