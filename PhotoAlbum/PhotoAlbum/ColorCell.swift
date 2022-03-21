9ff0329bc13d69ab23d4796926b9238af8d5f390
asqw887 <asqw887@gmail.com>
2022-03-21 21:31:35 +0900

    add - Color, ColorCell

---
+++ PhotoAlbum/PhotoAlbum/ColorCell.swift
@@ -0,0 +1,18 @@
+//
+//  ColorCell.swift
+//  PhotoAlbum
+//
+//  Created by 최예주 on 2022/03/21.
+//
+
+import Foundation
+
+class ColorCell {
+    
+    private var color: Color
+    
+    init() {
+        color = Color.random()
+    }
+    
+}
