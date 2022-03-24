22fe99d5be21321c28d5757a18bb9be4b2b2e9d7
sungju-kim <wnwn0719@gmail.com>
2022-03-24 21:09:37 +0900

    chore - directory 삭제

---
+++ PhotoAlbum/PhotoAlbum/Photos/PhotoManager.swift
@@ -0,0 +1,66 @@
+//
+//  PhotoResult.swift
+//  PhotoAlbum
+//
+//  Created by 최예주 on 2022/03/24.
+//
+
+import Foundation
+import Photos
+import UIKit
+
+protocol PhotoManagerDelegate {
+    func photoManager(didLoad data: [Data])
+}
+
+class PhotoManager: NSObject, PHPhotoLibraryChangeObserver{
+    
+    var delegate: PhotoManagerDelegate?
+    
+    private var fetchResult: PHFetchResult<PHAsset>?
+    private let imageManager: PHCachingImageManager = PHCachingImageManager()
+    
+    
+    override init() {
+        super.init()
+        PHPhotoLibrary.shared().register(self)
+    }
+    
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+    }
+    
+    private func fetchPhotosData(){
+        let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
+        guard let cameraRollCollection = cameraRoll.firstObject else { return }
+        
+        let allPhotosOptions = PHFetchOptions()
+        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: allPhotosOptions)
+    }
+    
+    private func convertToData(with asset: PHAsset) -> Data {
+        var convertedData: Data = Data()
+        let requestOption = PHImageRequestOptions()
+        requestOption.isSynchronous = true
+        
+        self.imageManager.requestImageDataAndOrientation(for: asset, options: requestOption) { data, _, _, _ in
+            guard let imageData = data else { return }
+            convertedData = imageData
+        }
+        
+        return convertedData
+    }
+    
+    func loadPhotoLibrary() {
+        var data: [Data] = []
+        fetchPhotosData()
+        if let fetchResult = fetchResult {
+            for asset in 0..<fetchResult.count{
+                data.append(convertToData(with: fetchResult.object(at: asset)))
+            }
+        }
+        delegate?.photoManager(didLoad: data)
+    }
+    
+    
+}
